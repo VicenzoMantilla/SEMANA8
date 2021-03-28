cb1d5e8d6261b6391e42e5a7f010ea017829f86a
VicenzoMantilla <vicenzomantilla94@gmail.com>
2021-03-28 18:39:00 -0300

    Exercise-01

---
+++ Desktop/RADIUMROCKET/SEMANA8/Exercise-01.js
@@ -0,0 +1,10 @@
+
+/*Create two numeric variables and use the sum operator to store
+ the sum value of both numbers in a 3rd variable*/
+
+ const Number1=2;
+ const Number2=3;
+
+ var SumTotal= Number1 + Number2;
+ 
+ console.log("The result is: " + SumTotal)
