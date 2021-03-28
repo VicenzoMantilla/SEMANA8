062d49b5751b87498984c952758eacf9f806f8e9
VicenzoMantilla <vicenzomantilla94@gmail.com>
2021-03-28 19:30:12 -0300

    Exercise-05

---
+++ Desktop/RADIUMROCKET/SEMANA8/Exercise-05.js
@@ -0,0 +1,10 @@
+/* Strings:
+Create a string variable with at least 10 characters and generate
+a new string with the first 5 characters saving the result in a new variable (use substring).
+*/
+
+var text1='enciclopedia';
+
+var text2= text1.substring(0,5);
+
+console.log('The new word is: ' + text2);
