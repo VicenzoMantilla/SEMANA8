c9cd1e8052b0c972635b023f4c92b2de0235c7a6
VicenzoMantilla <vicenzomantilla94@gmail.com>
2021-03-28 19:16:28 -0300

    Exercise-03

---
+++ Desktop/RADIUMROCKET/SEMANA8/Exercise-03.js
@@ -0,0 +1,11 @@
+/*
+Create two String variables and add the length
+ of each variable (number of letters in the string) and store the result in a 3rd variable (use length).
+ */
+
+ var text1='Telescopio';
+ var text2='Celular';
+
+ var result= text1 + text2;
+
+ console.log(' The lengt of "result" is: ' + result.length);
