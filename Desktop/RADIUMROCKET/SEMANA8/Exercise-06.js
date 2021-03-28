29aa8a0bdafc8ae7c60454b045d889377db3e401
VicenzoMantilla <vicenzomantilla94@gmail.com>
2021-03-28 19:36:37 -0300

    Exercise-06

---
+++ Desktop/RADIUMROCKET/SEMANA8/Exercise-06.js
@@ -0,0 +1,10 @@
+/* Strings:
+Create a string variable with at least 10 characters
+and generate a new string with the last 3 characters saving the result in a new variable (use substring).
+*/
+
+var text1='Electrotecnia';
+
+var textf= text1.substring(10,13);
+
+console.log('The new word is: ' + textf);
