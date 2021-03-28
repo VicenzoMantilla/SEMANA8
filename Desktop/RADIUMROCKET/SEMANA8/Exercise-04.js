e173d056961ae44f575d8bdc5604deaf8e6f7533
VicenzoMantilla <vicenzomantilla94@gmail.com>
2021-03-28 19:24:04 -0300

    Exercise-04

---
+++ Desktop/RADIUMROCKET/SEMANA8/Exercise-04.js
@@ -0,0 +1,9 @@
+/* Strings:
+Create a string variable with at least 10 characters and convert all text to uppercase (use toUpperCase).
+*/
+
+var text1='telescopio';
+
+var text2= text1.toUpperCase();
+
+console.log(text2);
