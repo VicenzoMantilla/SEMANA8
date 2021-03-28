4cd2f8f595da9e41c96fea7bc30c439106f0877d
VicenzoMantilla <vicenzomantilla94@gmail.com>
2021-03-28 19:09:24 -0300

    Exercise-02

---
+++ Desktop/RADIUMROCKET/SEMANA8/Exercise-02.js
@@ -0,0 +1,10 @@
+/* 
+Create two String variables and concatenate them, saving the result in a 3rd variable.
+*/
+
+var text1='Hello,';
+var text2=' how you doing?';
+
+var question= text1 + text2 ;
+
+console.log(question);
