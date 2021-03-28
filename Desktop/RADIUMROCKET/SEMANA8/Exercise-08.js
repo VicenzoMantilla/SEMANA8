c044c77f63bbc184b1042e781a0fcbea5d37903c
VicenzoMantilla <vicenzomantilla94@gmail.com>
2021-03-28 20:10:52 -0300

    Exercise-08

---
+++ Desktop/RADIUMROCKET/SEMANA8/Exercise-08.js
@@ -0,0 +1,11 @@
+/* Strings
+Create a string variable with at least 10 characters and some white space. 
+Find the position of the first blank space and store it in a variable (use indexOf).
+*/
+
+var text1='Ingenieria Industrial';
+var text2=' ';
+
+var result= text1.indexOf(text2);
+
+console.log('The blank space is in position: ' + result);
