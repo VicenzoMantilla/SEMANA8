74062f09be1c57836eb042ed09dab9fc261d6b61
VicenzoMantilla <vicenzomantilla94@gmail.com>
2021-03-28 21:44:58 -0300

    Exercise-15

---
+++ Desktop/RADIUMROCKET/SEMANA8/Exercise-15.js
@@ -0,0 +1,9 @@
+/*Arrays
+Join all elements of the array into a single string where each month is separated by a hyphen - (use join).
+*/
+
+var meses=["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+var mesesunidos= meses.join('-');
+
+console.log(mesesunidos);
