af37516a75aa87e9850e3dcf096e153012f998c9
VicenzoMantilla <vicenzomantilla94@gmail.com>
2021-03-28 21:25:38 -0300

    Exercise-11

---
+++ Desktop/RADIUMROCKET/SEMANA8/Exercise-11.js
@@ -0,0 +1,7 @@
+/* Arrays
+Sort the months array alphabetically and display it by console (use sort).
+*/
+
+var meses=["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+console.log(meses.sort());
