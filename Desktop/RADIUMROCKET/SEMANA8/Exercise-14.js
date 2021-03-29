fd1cd768dc4e2b40d65ede6ee06e464d9b8be4ff
VicenzoMantilla <vicenzomantilla94@gmail.com>
2021-03-28 21:41:37 -0300

    Exercise-14

---
+++ Desktop/RADIUMROCKET/SEMANA8/Exercise-14.js
@@ -0,0 +1,9 @@
+/* Arrays
+Reverse array order (use reverse)
+*/
+
+var meses=["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+var meses2= meses.reverse();
+
+console.log(meses2);
