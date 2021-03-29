7583f999934e388eb25a7ac27f4b5902c6279f80
VicenzoMantilla <vicenzomantilla94@gmail.com>
2021-03-28 21:48:00 -0300

    Exercise-16

---
+++ Desktop/RADIUMROCKET/SEMANA8/Exercise-16.js
@@ -0,0 +1,9 @@
+/*
+Create a copy of the months array containing May through November (use slice).
+*/
+
+var meses=["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+var meses2= meses.slice(4,11);
+
+console.log(meses2);
