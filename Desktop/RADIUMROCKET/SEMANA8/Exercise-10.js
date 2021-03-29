3e63255a7fd7e7e18dfb210b649b5d6d34a66ad8
VicenzoMantilla <vicenzomantilla94@gmail.com>
2021-03-28 21:18:03 -0300

    Exercise-10

---
+++ Desktop/RADIUMROCKET/SEMANA8/Exercise-10.js
@@ -0,0 +1,9 @@
+/* Arrays
+Given the following array: 
+["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+show by console the months 5 and 11 (use console.log)
+*/
+
+var meses=["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+console.log(meses[4] + ' ' + meses [10]);
