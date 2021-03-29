41ac911cb7a81db64df51d927138ac98d2017c9f
VicenzoMantilla <vicenzomantilla94@gmail.com>
2021-03-28 21:36:14 -0300

    Exercise-12

---
+++ Desktop/RADIUMROCKET/SEMANA8/Exercise-12.js
@@ -0,0 +1,10 @@
+/* Arrays
+Add an element to the beginning and end of the array (use unshift and push).
+*/
+
+var meses=["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+var meses2=meses.push(["lunes"]);
+var meses3=meses.unshift(["martes"]);
+
+console.log(meses3);
