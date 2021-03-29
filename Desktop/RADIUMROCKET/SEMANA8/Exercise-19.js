76c5b4bd1e4990b91e97189e37945268a398d65d
VicenzoMantilla <vicenzomantilla94@gmail.com>
2021-03-28 23:19:36 -0300

    Exercise-19

---
+++ Desktop/RADIUMROCKET/SEMANA8/Exercise-19.js
@@ -0,0 +1,10 @@
+/* FOR 
+*/
+
+var semana=["Lunes", "Martes", "Miercoles","Jueves","Viernes"];
+var result=[];
+
+for (var i=0; i<(semana.length); i++){
+    result.push(semana[i]);
+}
+alert(result);
