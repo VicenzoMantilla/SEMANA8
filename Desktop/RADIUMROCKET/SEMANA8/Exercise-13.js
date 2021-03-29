436ac49b033adcefda54f737cd347a49ad437beb
VicenzoMantilla <vicenzomantilla94@gmail.com>
2021-03-28 21:39:30 -0300

    Exercise-13

---
+++ Desktop/RADIUMROCKET/SEMANA8/Exercise-13.js
@@ -0,0 +1,10 @@
+/* Arrays
+Remove an element from the beginning and end of the array (use shift and pop).
+*/
+
+var meses=["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+var meses2= meses.shift();
+var meses3= meses.pop();
+
+console.log(meses);
